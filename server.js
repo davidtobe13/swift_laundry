d10cf625632c8d59e962eff9c7f331b17cecdd39
davidtobe13 <davidtobe1999@gmail.com>
2024-02-23 12:15:28 +0100

    new update commit

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require("./config/config")
 const cors = require("cors")
 const userRouter = require("./routers/userRouter")
 const shopRouter = require("./routers/shopRouter")
+const deliveryRouter = require("./routers/deliveryRouter")
+const itemRouter = require("./routers/itemRouter")
+const mainOrderRouter = require("./routers/mainOrderRouter")
+const orderRouter = require("./routers/orderRouter")
 
 
 // create an app instance of express
@@ -11,8 +15,11 @@ app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(shopRouter)
-
+app.use(deliveryRouter)
+app.use(itemRouter)
+app.use(mainOrderRouter)
+app.use(orderRouter)
 // check if the server is connected
 app.listen(process.env.PORT,()=>{
-    console.log(`server on port: ${process.env.PORT}`);
+    console.log(`server is connected on port: ${process.env.PORT}`);
 })
