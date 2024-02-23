4ba74be63e18bd8ce8ea80898fbd387e13512259
ekezieprosper <japhethprosper234@gmail.com>
2024-02-23 11:36:28 +0100

    user Router

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,17 +1,28 @@
 const router = require("express").Router()
 
-const {registerUser,signIn,signOut,forgotPassword,resetPasswordPage,resetPassword} = require("../controllers/userController")
+const {registerUser,signIn,signOut,forgotPassword,resetPasswordPage,resetPassword,
+    updateUser,getAllOrders,getAllPendingOrders, getAllCompletedOrders,getOneOrder, getOneShop, getAllShop,
+    userSilverPlan, userGoldPlan} = require("../controllers/userController")
 const authenticate = require("../middlewares/authentication")
 const userValidation = require("../middlewares/userVal")
 const myValidation = require("../middlewares/validation")
 // const upload = require("../utils/multer")
 
-router.post("/register-user", userValidation,registerUser)
-router.post("/signin",authenticate ,signIn)
-router.post("/signout",authenticate ,signOut)
-router.get("/forgetpassword" ,forgotPassword)
-router.get("/resetpasswordpage", resetPasswordPage)
-router.put("/resetpassword", userValidation, resetPassword)
+router.post("/register-User", userValidation,registerUser)
+router.post("/sign-In",signIn)
+router.post("/sign-Out",authenticate ,signOut)
+router.get("/forget-Password" ,forgotPassword)``
+router.get("/reset-Password-Page/:id", resetPasswordPage)
+router.put("/reset-Password/:id", resetPassword)
+router.put("/update-User/:id", authenticate, updateUser)
+router.get("/get-All-Orders/:userId", authenticate, getAllOrders)
+router.get("/get-All-Pending-Orders",authenticate, getAllPendingOrders)
+router.get("/get-All-Completed-Orders",authenticate, getAllCompletedOrders)
+router.get("/get-One-Order/:userId/:orderId",authenticate, getOneOrder)
+router.get("/get-One-Shop/:/userId:shopId ", authenticate,getOneShop )
+router.get("/get-All-Shop/:userId", authenticate,getAllShop)
+router.get("/user-Silver-Plan",authenticate, userSilverPlan)
+router.get("/user-Gold-Plan",authenticate, userGoldPlan)
 
 
 module.exports = router
