38d95b367f45bb5092d59247ffbf1682e45ab258
davidtobe13 <davidtobe1999@gmail.com>
2024-03-06 07:28:09 +0100

    ff

--- controllers/mainOrderController.js
+++ controllers/mainOrderController.js
@@ -60,6 +60,31 @@ exports.createUserOrder = async (req, res) => {
 };
 
 
+exports.getAllMainOrders = async (req, res) =>{
+    try{
+        const {userId} = req.user
+        const id = req.params.id
+        const user = await userModel.findById(userId)
+        if(!user){
+            return res.status(404).json({
+                error: `User not found`
+            })
+        }
+        const mainOrder = await mainOrderModel.findById(id)
+        if(!mainOrder){
+            return res.status(404).json({
+                error: `Order not found`
+            })
+        }
+        res.status(200).json({
+            message: `Orders found`,
+            data: mainOrder
+        })
+    }catch(error){
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
 
 exports.getAddedOrder = async (req, res) =>{
     try{
--- routers/mainOrderRouter.js
+++ routers/mainOrderRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createUserOrder, getAddedOrder } = require('../controllers/mainOrderController');
+const { createUserOrder, getAddedOrder, getAllMainOrders } = require('../controllers/mainOrderController');
 const { authenticate } = require('../middlewares/authentication');
 const router = express.Router();
 
 router.post('/create-user-order', authenticate, createUserOrder);
 router.get("/get-added-order", authenticate, getAddedOrder);
+router.get('/get-all-main-order', authenticate, getAllMainOrders)
 
 module.exports = router;
