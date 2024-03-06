9f0541962a3230da494c3f81459feed94180d63b
davidtobe13 <davidtobe1999@gmail.com>
2024-03-06 06:45:51 +0100

    fff

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -123,3 +123,30 @@ exports.deleteOrder = async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 };
+
+
+exports.getAllOrder = async (req, res) =>{
+    try{
+        const {userId} = req.user
+
+        const user = await userModel.findById(userId)
+        if(!user){
+            return res.status(404).json({
+                error: `User not found`
+            })
+        }
+
+        const order = await orderModel.find()
+        if(!order){
+            return res.status(404).json({
+                    error: `Order not found`
+                })
+        }
+        res.status(200).json({
+            message: `All orders found`,
+            data: order
+        })
+    }catch(error){
+        error: `Internal server error: ${error.message}`;
+    }
+}
--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { deleteOrder, createOrder } = require('../controllers/orderController');
+const { deleteOrder, createOrder, getAllOrder } = require('../controllers/orderController');
 const { authenticate } = require('../middlewares/authentication');
 const router = express.Router();
 
 router.post('/create-order', authenticate, createOrder );
 router.delete('/delete-order/:id',authenticate, deleteOrder)
+router.get('/get-all-order', authenticate, getAllOrder)
 
 module.exports = router;
