6c552e34220e61fd6d6b300f8ce2c48080b73ae1
davidtobe13 <davidtobe1999@gmail.com>
2024-03-07 02:50:44 +0100

    ff

--- controllers/userController.js
+++ controllers/userController.js
@@ -597,24 +597,24 @@ exports.getAllShop = async (req, res) => {
         }
 
         // Convert user's address to coordinates
-        const userCoordinates = await convertAddressToCoordinates(user.address);
+        // const userCoordinates = await convertAddressToCoordinates(user.address);
         // if (!userCoordinates) {
         //     // Request user to turn on device location
         //     return res.status(400).json({ error: 'Please turn on your device location' });
         // }
 
-        // let userCoordinates;
-        // // Check if user's location is available
-        // if (req.user.location) {
-        //     userCoordinates = req.user.location;
-        // } else {
-        //     // Convert user's address to coordinates
-        //     userCoordinates = await convertAddressToCoordinates(user.address);
-        //     if (!userCoordinates) {
-        //         // Request user to turn on device location
-        //         return res.status(400).json({ error: 'Please turn on your device location' });
-        //     }
-        // }
+        let userCoordinates;
+        // Check if user's location is available
+        if (req.user.location) {
+            userCoordinates = req.user.location;
+        } else {
+            // Convert user's address to coordinates
+            userCoordinates = await convertAddressToCoordinates(user.address);
+            if (!userCoordinates) {
+                // Request user to turn on device location
+                return res.status(400).json({ error: 'Please turn on your device location' });
+            }
+        }
         // Convert shop addresses to coordinates and calculate distances
         const shopsWithDistances = [];
         for (const shop of shops) {
